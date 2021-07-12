a2d63fac4fe7b18e62f92f2f847f408114c058de
vision20400 <vision20400@naver.com>
2021-07-12 20:19:58 +0900

    쌤이 수정함

--- app/src/main/java/com/sandburg/car/MainActivity.java
+++ app/src/main/java/com/sandburg/car/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity { //cn
+public class MainActivity extends AppCompatActivity { //cn 멍청이
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
